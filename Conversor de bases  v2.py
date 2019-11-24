0e06f177853e0988938756707972c06810185216
strfloat <58152339+strfloat@users.noreply.github.com>
2019-11-24 23:02:04 +0000

    Add files via upload

---
+++ Conversor de bases  v2.py
@@ -0,0 +1,63 @@
+##################
+### RUI SANTOS ###
+##################
+
+intro='''
+    ####################################################
+    ### ISTO É UM CONVERSOR DE SISTEMAS DE NUMERAÇÃO ###
+    ####################################################
+    '''
+print(intro)
+bbit=str(input('Qual a base do número? '))
+bbit=bbit.upper()
+
+if bbit==('BINARIO') or bbit==('BINÁRIO'):
+    bit=str(input('Qual o número binário a converter? '))
+    bcbit=str(input('Converter o número binário para qual base? '))
+    bcbit=bcbit.upper()
+    if bcbit==('DECIMAL'):
+        print(int(bit, 2))
+    #detetar erro esta a dar números a mais
+    if bcbit==('OCTAL'):
+        b=int(bit, 2)
+        print(oct(b))
+    if bcbit==('HEXADECIMAL'):
+        b=int(bit, 2)
+        print(hex(b))
+if bbit==('OCTAL'):
+    bit=str(input('Qual o número octal a converter? '))
+    bcbit=str(input('Converter o número octal para qual base? '))
+    bcbit=bcbit.upper()
+    if bcbit==('DECIMAL'):
+        print(int(bit, 8))
+    #aparece numeros e letras a mais
+    if bcbit==('BINARIO') or bcbit==('BINÁRIO'):
+        b=int(bit, 8)
+        print(bin(b))
+    if bcbit==('HEXADECIMAL'):
+        b=int(bit, 8)
+        print(hex(b))
+if bbit==('HEXADECIMAL'):
+    bit=str(input('Qual o número hexadecimal a converter? '))
+    bcbit=str(input('Converter o número hexadecimal para qual base? '))
+    bcbit=bcbit.upper()
+    if bcbit==('DECIMAL'):
+        print(int(bit, 16))
+    if bcbit==('BINARIO') or bcbit==('BINÁRIO'):
+        b=int(bit, 16)
+        print(bin(b))
+    if bcbit==('OCTAL'):
+        b=int(bit, 16)
+        print(oct(b))
+if bbit==('DECIMAL'):
+    bit=int(input('Qual o número decimal a converter? '))
+    bcbit=str(input('Converter o número decimal para qual base? '))
+    bcbit=bcbit.upper()
+    if bcbit==('HEXADECIMAL'):
+        print(hex(bit))
+    if bcbit==('BINARIO') or bcbit==('BINÁRIO'):
+        print(bin(bit))
+    if bcbit==('OCTAL'):
+        print(oct(bit))
+
+
